cd5904330d5ece575e90333d04a65de88fb02909
jedrazb <jedrazb@gmail.com>
2018-05-27 19:06:28 +0100

    [jb2616] redux dependency added, fixed scoping for private nmp registries

--- frontend/justpostme/src/App.js
+++ frontend/justpostme/src/App.js
@@ -3,31 +3,24 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
-type Props = {};
+const ActualApp = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to React</h1>
+    </header>
+    <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+  </div>
+);
 
-type State = {};
-
-class App extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super();
-    this.state = {
-      clicked: false
-    };
-  }
-
+class App extends React.Component<void> {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <ActualApp />;
   }
 }
 
