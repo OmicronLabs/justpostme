70d42c83e0c198336b9d45fa33e3fa5f632c9311
jedrazb <jedrazb@gmail.com>
2018-05-30 22:50:45 +0100

    [jb2616] Persisting state of the app between refreshes

--- frontend/justpostme/src/App.js
+++ frontend/justpostme/src/App.js
@@ -1,31 +1,24 @@
 //@flow
 
 import React, { Component } from "react";
-import thunkMiddleware from "redux-thunk";
-import { createLogger } from "redux-logger";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { createStore, applyMiddleware } from "redux";
-import rootReducer from "./store";
 import Routes from "./Routes";
+import { PersistGate } from "redux-persist/integration/react";
+import persistedStore from "./store";
 
-const loggerMiddleware = createLogger();
-
-const store = createStore(
-  rootReducer,
-  applyMiddleware(
-    thunkMiddleware, // lets us dispatch() functions
-    loggerMiddleware // neat middleware that logs actions
-  )
-);
+const { store, persistor } = persistedStore();
 
 class App extends React.Component<void> {
   render() {
     return (
       <Provider store={store}>
-        <Router>
-          <Route component={Routes} />
-        </Router>
+        <PersistGate loading={null} persistor={persistor}>
+          <Router>
+            <Route component={Routes} />
+          </Router>
+        </PersistGate>
       </Provider>
     );
   }
--- frontend/justpostme/src/store.js
+++ frontend/justpostme/src/store.js
@@ -1,12 +1,39 @@
 //@flow
 
 import { combineReducers } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import { createStore, applyMiddleware } from "redux";
+import thunkMiddleware from "redux-thunk";
+import { createLogger } from "redux-logger";
+import storage from "redux-persist/lib/storage";
 import user from "./reducers/user";
 import managedPages from "./reducers/managedPages";
 import unmanagedPages from "./reducers/unmanagedPages";
 
-export default combineReducers({
+const loggerMiddleware = createLogger();
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["user"]
+};
+
+const rootReducer = combineReducers({
   user,
   managedPages,
   unmanagedPages
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export default () => {
+  let store = createStore(
+    persistedReducer,
+    applyMiddleware(
+      thunkMiddleware, // lets us dispatch() functions
+      loggerMiddleware // neat middleware that logs actions
+    )
+  );
+  let persistor = persistStore(store);
+  return { store, persistor };
+};
