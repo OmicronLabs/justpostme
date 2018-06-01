5c382033c8d78f6fa5f27e898c3839bae021a8bb
mhutti1 <mhutti1@gmail.com>
2018-06-01 09:05:23 +0000

    Add frontend http -> https redirect

--- frontend/justpostme/server.js
+++ frontend/justpostme/server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express(); 
 var https = require('https');
 var fs = require('fs');
-
+var http = require('http');
 
 var privateKey  = fs.readFileSync('privkey.pem', 'utf8');
 var certificate = fs.readFileSync('fullchain.pem', 'utf8');
@@ -27,3 +27,13 @@ server.listen(443, function(){
     console.log("server running at https://IP_ADDRESS:8001/")
 });
 
+
+// set up http redirect
+var http = express.createServer();
+
+// set up a route to redirect http to https
+http.get('*', function(req, res) {
+    res.redirect('https://' + req.headers.host + req.url);
+})
+
+http.listen(80);
