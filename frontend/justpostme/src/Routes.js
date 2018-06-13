d26c52926fb3d93a160492357574ed397d7b4001
Boris Barath <boris.barath16@imperial.ac.uk>
2018-06-13 19:11:10 +0100

    Restored conditional navigation

--- frontend/justpostme/src/Routes.js
+++ frontend/justpostme/src/Routes.js
@@ -17,7 +17,12 @@ class Routes extends React.Component {
       <div>
         <Snackbar showSnackbar={showSnackbar}>{snackbarMessage}</Snackbar>
         <Switch>
-          <Route path="/login" component={WelcomePage} />
+          <Route
+            path="/login"
+            component={props => {
+              return loggedIn ? <ManagePages /> : <WelcomePage />;
+            }}
+          />
           <Route path="/about" component={AboutPage} />
           <Route
             path="/pages"
